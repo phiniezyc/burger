5bdb63495d1f77b0888c463dbfdbde882de3af9d
Chance Phiniezy <phiniezyc@gmail.com>
2017-11-17 01:54:43 -0500

    adding a burger adds it to the database, but when the burger is added it shows undefined and is not displaying to the page

--- config/orm.js
+++ config/orm.js
@@ -48,10 +48,10 @@ var orm = {
         });
     },
     create: function (tableInput, val, cb) {
-        connection.query('INSERT INTO '+tableInput+" (burger_name) VALUES ("+val+");", function(err, result){
+        connection.query('INSERT INTO '+tableInput+" (burger_name) VALUES ('"+val+"');", function(err, result){
             if(err) throw err;
             cb(result);
-        })
+        });
     },
     update: function (tableInput, condition, cb) {
         connection.query("UPDATE " + tableInput + " SET devoured=true WHERE id=" + condition + ";", function (err, result) {
--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -3,53 +3,32 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
-var burger = require("../models/burger");
+var burger = require("../models/burger.js");
 
 //Create all our routes and set up logic within those routes where required.
 router.get("/", function (req, res) {
-    burger.all(function(burger_data){
+    burger.all(function (burger_data) {
         console.log(burger_data);
-        res.render("index", {burger_data});
+        res.render("index", {
+            burger_data
+        });
     });
 });
-
-router.put("/burgers/update", function (req, res){
-    burger.update(req.body.burger_id, function(result){
+router.put("/burgers/update", function (req, res) {
+    burger.update(req.body.burger_id, function (result) {
         console.log(result);
         res.redirect("/");
     });
 });
-
-// router.post("/api/burgers", function (req, res) {
-//     console.log(res.body);
-//     console.log(res.body);
-//     res.send("test");
-
-//     burger.create([
-//         "name"
-//     ], [
-//         req.body.name,
-//     ], function (result) {
-//         // Send back the ID of the new quote
-//         res.json({
-//             id: result.insertId
-//         });
-//     });
-// });
+router.post("/burgers/create", function (req, res) {
+            burger.create(req.body.burger_data, function (result) {
+                console.log(result);
+                res.redirect("/");
+            });
+        });
 
 
-// router.delete("/api/cats/:id", function (req, res) {
-//     var condition = "id = " + req.params.id;
 
-//     burger.delete(condition, function (result) {
-//         if (result.affectedRows == 0) {
-//             // If no rows were changed, then the ID must not exist, so 404
-//             return res.status(404).end();
-//         } else {
-//             res.status(200).end();
-//         }
-//     });
-// });
 
 // Export routes for server.js to use.
 module.exports = router;
